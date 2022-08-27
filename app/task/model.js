2e2906da9ebf69aed3751b314d0fee686427245b
MartaSzuran <marta.szuran@gmail.com>
2022-08-27 12:09:39 +0200

    Show tasks

--- app/task/model.js
+++ app/task/model.js
@@ -2,4 +2,6 @@ import Model, { attr } from '@ember-data/model';
 
 export default class UserModel extends Model {
   @attr('string') description;
+  @attr('boolean') isDone;
+  @attr('date') createdAt;
 }
