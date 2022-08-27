17e7d5c41e3e0fee56460f20ec6fb33afec5ca10
MartaSzuran <marta.szuran@gmail.com>
2022-08-27 18:58:20 +0200

    change url to my db

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,8 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class ApplicationAdapter extends RESTAdapter {
-  host = 'https://db2c-185-78-75-249.ngrok.io';
+  host = 'http://localhost:3000';
+
   get headers() {
     return { 'owner-secret': 'cV87zOVw3F' };
   }
