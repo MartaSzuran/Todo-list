620ec0a257ac7800347bdf94e27236e98b58ecc3
MartaSzuran <marta.szuran@gmail.com>
2022-08-27 11:33:59 +0200

    catch data from https

---
+++ app/application/adapter.js
@@ -0,0 +1,8 @@
+import RESTAdapter from '@ember-data/adapter/rest';
+
+export default class ApplicationAdapter extends RESTAdapter {
+  host = 'https://db2c-185-78-75-249.ngrok.io';
+  get headers() {
+    return { 'owner-secret': 'cV87zOVw3F' };
+  }
+}
