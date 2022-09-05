8fd962f6e92e7da49094dd97e0456ac0b083d171
MartaSzuran <marta.szuran@gmail.com>
2022-09-05 20:01:01 +0200

    removing helper for switch checked validation, cleaning up some redundance code

--- app/components/shared/switch/component.js
+++ app/components/shared/switch/component.js
@@ -2,10 +2,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class SharedSwitchComponent extends Component {
-  get isSwitchON() {
-    return this.args.checked;
-  }
-
   @action
   onClick() {
     this.args?.onClick?.();
